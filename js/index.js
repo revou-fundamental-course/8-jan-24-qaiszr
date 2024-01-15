abeaf799f01c8363574cc6d076ecfe5a091b3039
Vynx x <qaiszahranrabbani@gmail.com>
2024-01-15 23:06:30 +0700

    tugas revou tes (perubahan)

---
+++ js/index.js
@@ -0,0 +1,15 @@
+function luas(){
+    var s = document.getElementById('s').value;
+
+    var luas = s*s;
+
+    document.getElementById('luas').innerHTML = luas;
+}
+
+function luas(){
+    var s = document.getElementById('s').value;
+
+    var kel = 4*s;
+
+    document.getElementById('kel').innerHTML = kel;
+}
