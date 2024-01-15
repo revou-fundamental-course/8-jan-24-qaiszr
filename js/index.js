5755ac794076ba0ba67ce8568a16dc0e5bad13cb
Vynx x <qaiszahranrabbani@gmail.com>
2024-01-16 01:03:15 +0700

    Mini Project ReVou - Qais

--- js/index.js
+++ js/index.js
@@ -2,14 +2,14 @@ function luas(){
     var s = document.getElementById('s').value;
 
     var luas = s*s;
-
+    
     document.getElementById('luas').innerHTML = luas;
 }
 
-function luas(){
+function kel(){
     var s = document.getElementById('s').value;
 
     var kel = 4*s;
-
+    
     document.getElementById('kel').innerHTML = kel;
 }
